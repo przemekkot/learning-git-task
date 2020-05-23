1f0d23c5579b6728cdc06b4d381ba35f5ee84a95
Przemyslaw Kot <przemekkot@wp.pl>
2020-05-23 10:02:01 +0200

    End project

--- shopping_list.py
+++ shopping_list.py
@@ -3,10 +3,16 @@ shopping_list = {
   "warzywniak": ["marchew", "seler", "rukola"]
 }
 
-print(shopping_list)
+# print(shopping_list)
+
+n = 0
 
 for shop in shopping_list:
     for i in range(len(shopping_list[shop])):
         shopping_list[shop][i]=shopping_list[shop][i].capitalize()
     print(f"Idę do {shop.capitalize()} i kupuję tam {shopping_list[shop]}")
     items=len(shopping_list[shop])
+    n = n + len(shopping_list[shop])
+    
+print(f"W sumie kupuję {n} produktów.")
+
