464e01609d00cede405133512f1599b573473389
Przemyslaw Kot <przemekkot@wp.pl>
2020-05-23 09:59:13 +0200

    Add count

--- shopping_list.py
+++ shopping_list.py
@@ -4,3 +4,9 @@ shopping_list = {
 }
 
 print(shopping_list)
+
+for shop in shopping_list:
+    for i in range(len(shopping_list[shop])):
+        shopping_list[shop][i]=shopping_list[shop][i].capitalize()
+    print(f"Idę do {shop.capitalize()} i kupuję tam {shopping_list[shop]}")
+    items=len(shopping_list[shop])
